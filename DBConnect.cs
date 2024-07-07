93f53ab8e6ed98fea26b7d4aaff12cd65710a47f
shuvo8220 <134973875+shuvo8220@users.noreply.github.com>
2024-07-07 14:12:27 +0600

    Add files via upload

---
+++ DBConnect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace KnockDoc
+{
+    internal class DBConnect
+    {
+        private SqlConnection con = new SqlConnection(@"Data Source=DARKPHANTOM\SQLEXPRESS;Initial Catalog=knock_doc;Integrated Security=True;");
+        public SqlConnection GetCon()
+        {
+            return con; 
+        } 
+        public void OpenCon()
+        {
+            if(con.State==ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+        public void CloseCon()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ DocInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnockDoc
+{
+    public partial class DocInfo : Form
+    {
+        public DocInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           this.Hide();
+            patientDashboard pa = new patientDashboard();   
+            pa.Show();
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            User_Information fm = new User_Information();
+            fm.Show();
+        }
+    }
+}
