c88ae8c54ea5635f99edd57f567b903f0a957bc8
shuvo8220 <134973875+shuvo8220@users.noreply.github.com>
2024-07-07 14:16:34 +0600

    Add files via upload

---
+++ User Information.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace KnockDoc
+{
+    partial class User_Information
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(User_Information));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Address = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPname = new System.Windows.Forms.TextBox();
+            this.txtPaddress = new System.Windows.Forms.TextBox();
+            this.txtPnumber = new System.Windows.Forms.TextBox();
+            this.txtPbgroup = new System.Windows.Forms.TextBox();
+            this.txtPproblem = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPage = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.btnPsave = new System.Windows.Forms.Button();
+            this.PatientList = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PatientList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(214, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Name";
+            // 
+            // Address
+            // 
+            this.Address.AutoSize = true;
+            this.Address.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Address.Location = new System.Drawing.Point(214, 114);
+            this.Address.Name = "Address";
+            this.Address.Size = new System.Drawing.Size(70, 23);
+            this.Address.TabIndex = 1;
+            this.Address.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(214, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Phone Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(214, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Blood Group";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(214, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Problems";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtPname
+            // 
+            this.txtPname.AccessibleName = "txtPname";
+            this.txtPname.Location = new System.Drawing.Point(366, 72);
+            this.txtPname.Name = "txtPname";
+            this.txtPname.Size = new System.Drawing.Size(302, 22);
+            this.txtPname.TabIndex = 5;
+            // 
+            // txtPaddress
+            // 
+            this.txtPaddress.AccessibleName = "txtPaddress";
+            this.txtPaddress.Location = new System.Drawing.Point(366, 115);
+            this.txtPaddress.Multiline = true;
+            this.txtPaddress.Name = "txtPaddress";
+            this.txtPaddress.Size = new System.Drawing.Size(302, 62);
+            this.txtPaddress.TabIndex = 6;
+            // 
+            // txtPnumber
+            // 
+            this.txtPnumber.AccessibleName = "txtPnumber";
+            this.txtPnumber.Location = new System.Drawing.Point(366, 203);
+            this.txtPnumber.Name = "txtPnumber";
+            this.txtPnumber.Size = new System.Drawing.Size(302, 22);
+            this.txtPnumber.TabIndex = 7;
+            // 
+            // txtPbgroup
+            // 
+            this.txtPbgroup.AccessibleName = "txtPbgroup";
+            this.txtPbgroup.Location = new System.Drawing.Point(366, 245);
+            this.txtPbgroup.Name = "txtPbgroup";
+            this.txtPbgroup.Size = new System.Drawing.Size(104, 22);
+            this.txtPbgroup.TabIndex = 8;
+            // 
+            // txtPproblem
+            // 
+            this.txtPproblem.AccessibleName = "txtPproblem";
+            this.txtPproblem.Location = new System.Drawing.Point(366, 390);
+            this.txtPproblem.Multiline = true;
+            this.txtPproblem.Name = "txtPproblem";
+            this.txtPproblem.Size = new System.Drawing.Size(284, 74);
+            this.txtPproblem.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(154, 640);
+            this.panel1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(8, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 34);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(488, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 23);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Age";
+            // 
+            // txtPage
+            // 
+            this.txtPage.AccessibleName = "txtPage";
+            this.txtPage.Location = new System.Drawing.Point(544, 245);
+            this.txtPage.Name = "txtPage";
+            this.txtPage.Size = new System.Drawing.Size(124, 22);
+            this.txtPage.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(214, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 23);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Gender";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label7.Location = new System.Drawing.Point(211, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(290, 38);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Fill your information";
+            // 
+            // radioMale
+            // 
+            this.radioMale.AccessibleName = "radioMale";
+            this.radioMale.AutoSize = true;
+            this.radioMale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioMale.Location = new System.Drawing.Point(403, 296);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(66, 24);
+            this.radioMale.TabIndex = 17;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Male";
+            this.radioMale.UseVisualStyleBackColor = true;
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AccessibleName = "radioFemale";
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioFemale.Location = new System.Drawing.Point(529, 296);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(85, 24);
+            this.radioFemale.TabIndex = 18;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Female";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // btnPsave
+            // 
+            this.btnPsave.AccessibleName = "btnPsave";
+            this.btnPsave.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnPsave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPsave.ForeColor = System.Drawing.Color.White;
+            this.btnPsave.Location = new System.Drawing.Point(365, 483);
+            this.btnPsave.Name = "btnPsave";
+            this.btnPsave.Size = new System.Drawing.Size(211, 39);
+            this.btnPsave.TabIndex = 19;
+            this.btnPsave.Text = "Submit";
+            this.btnPsave.UseVisualStyleBackColor = false;
+            this.btnPsave.Click += new System.EventHandler(this.btnPsave_Click);
+            // 
+            // PatientList
+            // 
+            this.PatientList.BackgroundColor = System.Drawing.Color.White;
+            this.PatientList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.PatientList.Location = new System.Drawing.Point(169, 537);
+            this.PatientList.Name = "PatientList";
+            this.PatientList.RowHeadersWidth = 51;
+            this.PatientList.RowTemplate.Height = 24;
+            this.PatientList.Size = new System.Drawing.Size(644, 91);
+            this.PatientList.TabIndex = 21;
+            this.PatientList.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.PatientList_CellClick);
+            this.PatientList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.PatientList_CellContentClick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(368, 345);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(278, 22);
+            this.dateTimePicker1.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(215, 345);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 20);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Date";
+            // 
+            // User_Information
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(825, 640);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.PatientList);
+            this.Controls.Add(this.btnPsave);
+            this.Controls.Add(this.radioFemale);
+            this.Controls.Add(this.radioMale);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtPproblem);
+            this.Controls.Add(this.txtPbgroup);
+            this.Controls.Add(this.txtPnumber);
+            this.Controls.Add(this.txtPaddress);
+            this.Controls.Add(this.txtPname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Address);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "User_Information";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "patient_Information";
+            this.Load += new System.EventHandler(this.User_Information_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PatientList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Address;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPname;
+        private System.Windows.Forms.TextBox txtPaddress;
+        private System.Windows.Forms.TextBox txtPnumber;
+        private System.Windows.Forms.TextBox txtPbgroup;
+        private System.Windows.Forms.TextBox txtPproblem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPage;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.Button btnPsave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView PatientList;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ User Information.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace KnockDoc
+{
+    public partial class User_Information : Form
+    {
+        private string connectionString = "Data Source=DARKPHANTOM\\SQLEXPRESS;Initial Catalog=knock_doc;Integrated Security=True;";
+        private int selectedp_id = 0;
+
+        public User_Information()
+        {
+            InitializeComponent();
+            patient();
+
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void User_Information_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+        private void patient()
+        {
+            try
+            {
+                string query = "SELECT * FROM patient_tbl";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        PatientList.DataSource = dataTable;
+                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error fetching data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+       
+
+       
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            patientDashboard fm = new patientDashboard();
+            fm.Show();
+        }
+       
+
+        private void btnPsave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (AreTextBoxesEmpty())
+                {
+                    MessageBox.Show("Missing Data!!!", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    string name = txtPname.Text;
+                    string address = txtPaddress.Text;
+                    string phone = txtPnumber.Text;
+                    string blood = txtPbgroup.Text;
+                    string problems = txtPproblem.Text;
+                    string age = txtPage.Text;
+                    string gender = string.Empty;
+                    DateTime selectedDateTime = dateTimePicker1.Value;
+
+                    if (radioMale.Checked)
+                    {
+                        gender = "Male";
+                    }
+                    else if (radioFemale.Checked)
+                    {
+                        gender = "Female";
+                    }
+
+                    
+                    string query = "INSERT INTO patient_tbl (p_name, p_address, p_phonenumber, p_blood,AppointmentDateTime, p_problems, p_gender) VALUES (@Name, @Address, @Phone, @Blood, @AppointmentDateTime,@Problems, @Gender)";
+
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+
+                        using (SqlCommand cmd = new SqlCommand(query, connection))
+                        {
+                            cmd.Parameters.AddWithValue("@Name", name);
+                            cmd.Parameters.AddWithValue("@Address", address);
+                            cmd.Parameters.AddWithValue("@Phone", phone);
+                            cmd.Parameters.AddWithValue("@Blood", blood);
+                            cmd.Parameters.AddWithValue("@Problems", problems);
+                            cmd.Parameters.AddWithValue("@Age", age);
+                            cmd.Parameters.AddWithValue("@Gender", gender);
+                            cmd.Parameters.AddWithValue("@AppointmentDateTime", selectedDateTime);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    patient();
+                    MessageBox.Show("Information Saved!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ClearTextBoxes();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error adding patient: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private bool AreTextBoxesEmpty()
+        {
+            
+            return string.IsNullOrEmpty(txtPname.Text) || string.IsNullOrEmpty(txtPaddress.Text) ||
+                   string.IsNullOrEmpty(txtPnumber.Text) || string.IsNullOrEmpty(txtPbgroup.Text) ||
+                   string.IsNullOrEmpty(txtPproblem.Text) || string.IsNullOrEmpty(txtPage.Text);
+        }
+
+        private void ClearTextBoxes()
+        {
+           
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox textBox)
+                {
+                    textBox.Clear();
+                }
+            }
+        }
+
+       
+
+        private void PatientList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+           
+        }
+
+        private void PatientList_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < PatientList.Rows.Count)
+            {
+                DataGridViewRow selectedRow = PatientList.Rows[e.RowIndex];
+
+                txtPname.Text = selectedRow.Cells["p_name"].Value?.ToString() ?? string.Empty;
+                txtPaddress.Text = selectedRow.Cells["p_address"].Value?.ToString() ?? string.Empty;
+                txtPnumber.Text = selectedRow.Cells["p_phonenumber"].Value?.ToString() ?? string.Empty;
+                txtPbgroup.Text = selectedRow.Cells["p_blood"].Value?.ToString() ?? string.Empty;
+                txtPproblem.Text = selectedRow.Cells["p_problems"].Value?.ToString() ?? string.Empty;
+                txtPage.Text = selectedRow.Cells["p_age"].Value?.ToString() ?? string.Empty;
+
+            
+                if (int.TryParse(selectedRow.Cells["p_id"].Value?.ToString(), out int p_id))
+                {
+                    selectedp_id = p_id;
+                }
+                else
+                {
+                    selectedp_id = 0; 
+                }
+            }
+            else
+            {
+                ClearTextBoxes(); 
+                selectedp_id = 0; 
+            }
+        }
+    }
+}
+
+
+// some features to work 
+//1. User Authentication; advanced datagridview;
